a3c3e8046bd66b1b9f514309e4abce8e9e5b13da
sijadkhan1678 <sijadkhan1678@gmail.com>
2022-07-22 08:28:12 +0500

    Updated Balance components

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import AccountSummary from './AccountSummary';
 
- const Balance = (heading) => {
-  return( <div className='header'>
-    <h3>Balance</h3>
-    <h1>$ 123</h1>
-   </div>)
+
+ const Balance = () => {
+
+  return (
+    <div className='Balance-container'>  
+     <div className='balance'>
+        <h3>Balance</h3>
+        <h1>$ 123</h1>
+       
+   </div>
+   
+   <AccountSummary  />
+   </div>
+
+   )
 }
 
 export default Balance;
